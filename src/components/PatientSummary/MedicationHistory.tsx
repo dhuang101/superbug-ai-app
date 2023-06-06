d0c7a3232a0c53097178271b59fdf313ae76583e
Derek Huang <derek.huang@monash.edu>
2023-06-06 11:16:48 +1000

    skeleton useeffect

--- src/components/PatientSummary/MedicationHistory.tsx
+++ src/components/PatientSummary/MedicationHistory.tsx
@@ -1,8 +1,12 @@
+import { useEffect } from "react"
+
 function MedicationHistory() {
+	useEffect(() => {}, [])
 	// component that generates each row
 	function TableRows() {
 		return <div></div>
 	}
+
 	return (
 		<div className="flex justify-center">
 			<table className="table w-11/12 [&_tr.hover:hover_*]:!bg-slate-300">
