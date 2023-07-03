77f3aa8801f2995409ea5dca9c36e9b39a42106c
dhuang101 <derek.huang@monash.edu>
2023-07-03 18:18:14 +1000

    Completed highlight and zoom function

--- src/components/AiModel/SubComponents/LineGraph.tsx
+++ src/components/AiModel/SubComponents/LineGraph.tsx
@@ -1,71 +1,192 @@
-import React from "react"
+import React, { useEffect, useReducer } from "react"
 import {
 	CartesianGrid,
 	Line,
 	LineChart,
+	ReferenceArea,
 	ResponsiveContainer,
 	Tooltip,
 	XAxis,
 	YAxis,
 } from "recharts"
 
-function LineGraph(props: { data: any[] }) {
+const initialState = {
+	data: [],
+	slicedData: [],
+	limit1: null,
+	limit2: null,
+}
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case "INITIALISE":
+			return {
+				data: action.data,
+				slicedData: action.data,
+				limit1: null,
+				limit2: null,
+			}
+		case "ADD_LIMIT":
+			if (state.limit1 === null) {
+				return { ...state, limit1: action.limit }
+			} else {
+				return { ...state, limit2: action.limit }
+			}
+		case "RESET_LIMITS":
+			return {
+				...state,
+				limit1: null,
+				limit2: null,
+			}
+		case "SLICE_DATA":
+			let limits = [state.limit1.index, state.limit2.index].sort()
+			let limitedList = state.slicedData.slice(limits[0], limits[1] + 1)
+			if (limitedList.length > 1) {
+				return {
+					...state,
+					slicedData: limitedList,
+					limit1: null,
+					limit2: null,
+				}
+			}
+		case "RESET_GRAPH":
+			return {
+				...state,
+				slicedData: state.data,
+				limit1: null,
+				limit2: null,
+			}
+		default:
+			return state
+	}
+}
+
+function LineGraph(props: any) {
+	const [graphState, dispatch] = useReducer(reducer, initialState)
+
+	useEffect(() => {
+		dispatch({ type: "INITIALISE", data: props.data })
+	}, [props])
+
+	// useEffect(() => {
+	// 	console.log(graphState)
+	// }, [graphState])
+
 	return (
 		<React.Fragment>
-			{props.data.length > 0 ? (
-				<ResponsiveContainer width={"100%"} height={"100%"}>
-					<LineChart
-						data={props.data}
-						margin={{
-							top: 5,
-							right: 41,
-							left: 20,
-							bottom: 5,
-						}}
-					>
-						<Line
-							type="monotone"
-							dataKey="value"
-							stroke="#3b82f6"
-						/>
-						<Tooltip
-							labelFormatter={(label) => {
-								return new Date(label).toLocaleString("en-AU", {
-									timeZone:
-										Intl.DateTimeFormat().resolvedOptions()
-											.timeZone,
-								})
-							}}
-							formatter={(value) => [
-								value + "%",
-								"Mortality Rate",
-							]}
-						/>
-						<CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
-						<XAxis
-							dataKey="issued"
-							dy={6}
-							tickFormatter={(tick) => {
-								return new Date(tick).toLocaleDateString(
-									"en-AU",
-									{
-										timeZone:
-											Intl.DateTimeFormat().resolvedOptions()
-												.timeZone,
-									}
-								)
-							}}
-						/>
-						<YAxis
-							tickFormatter={(tick) => {
-								return tick + "%"
-							}}
-						/>
-					</LineChart>
-				</ResponsiveContainer>
-			) : (
+			{props.data.length === 0 ? (
 				<div className="flex items-center justify-center h-[60vh] text-3xl">
 					Encounter Has No Observations
+				</div>
+			) : (
+				<div className="h-full">
+					<button
+						className="btn btn-sm rounded btn-primary mb-2 ml-20"
+						onClick={() => {
+							dispatch({
+								type: "RESET_GRAPH",
+							})
+						}}
+					>
+						Zoom Out
+					</button>
+					<ResponsiveContainer width={"100%"} height={"100%"}>
+						<LineChart
+							data={graphState.slicedData}
+							margin={{
+								top: 5,
+								right: 41,
+								left: 20,
+								bottom: 5,
+							}}
+							onMouseDown={(event: any) => {
+								if (event !== null) {
+									dispatch({
+										type: "ADD_LIMIT",
+										limit: {
+											value: event.activeLabel,
+											index: event.activeTooltipIndex,
+										},
+									})
+								}
+							}}
+							onMouseMove={(event: any) => {
+								if (event && graphState.limit1) {
+									dispatch({
+										type: "ADD_LIMIT",
+										limit: {
+											value: event.activeLabel,
+											index: event.activeTooltipIndex,
+										},
+									})
+								}
+							}}
+							onMouseUp={(event: any) => {
+								if (
+									graphState.limit1 !== null &&
+									graphState.limit2 !== null
+								) {
+									dispatch({
+										type: "SLICE_DATA",
+									})
+								} else {
+									dispatch({
+										type: "RESET_LIMITS",
+									})
+								}
+							}}
+						>
+							<Line
+								type="monotone"
+								dataKey="value"
+								stroke="#3b82f6"
+								animationDuration={300}
+							/>
+							<Tooltip
+								labelFormatter={(label) => {
+									return new Date(label).toLocaleString(
+										"en-AU",
+										{
+											timeZone:
+												Intl.DateTimeFormat().resolvedOptions()
+													.timeZone,
+										}
+									)
+								}}
+								formatter={(value) => [
+									value + "%",
+									"Mortality Rate",
+								]}
+							/>
+							<CartesianGrid
+								stroke="#ccc"
+								strokeDasharray="3 3"
+							/>
+							<XAxis
+								dataKey="issued"
+								dy={8}
+								tickFormatter={(tick) => {
+									return new Date(tick).toLocaleDateString(
+										"en-AU",
+										{
+											timeZone:
+												Intl.DateTimeFormat().resolvedOptions()
+													.timeZone,
+										}
+									)
+								}}
+							/>
+							<YAxis unit={"%"} />
+							{graphState.limit1 !== null &&
+							graphState.limit2 !== null ? (
+								<ReferenceArea
+									x1={graphState.limit1.value}
+									x2={graphState.limit2.value}
+									strokeOpacity={0.3}
+								/>
+							) : null}
+						</LineChart>
+					</ResponsiveContainer>
 				</div>
 			)}
 		</React.Fragment>
