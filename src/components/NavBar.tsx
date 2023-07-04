72fad68e33969bb13f4f90dd9fbe01a003df47f8
dhuang101 <derek.huang@monash.edu>
2023-07-04 10:07:47 +1000

    Fixed wrapping of home button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,9 +4,11 @@ function NavBar() {
 	return (
 		<div className="navbar bg-slate-900">
 			<div className="flex-1">
-				<div className="btn btn-ghost normal-case text-xl text-white">
-					<Link href={"/"}>Superbug AI</Link>
-				</div>
+				<Link href={"/"}>
+					<div className="btn btn-ghost normal-case text-xl text-white">
+						Superbug AI
+					</div>
+				</Link>
 			</div>
 			{/* <div className="flex-2">
 				<div className="btn btn-ghost normal-case text-xl text-white">
