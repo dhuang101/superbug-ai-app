1f7aaabe465e7d44e2f4255898a9e712a67e0819
Derek Huang <derek.huang@monash.edu>
2023-08-14 12:22:46 +1000

    Base logic for swapping theme

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,18 +2,32 @@ import Link from "next/link"
 import LightModeIcon from "@mui/icons-material/LightMode"
 import DarkModeIcon from "@mui/icons-material/DarkMode"
 
-function NavBar() {
+function NavBar(props) {
+	function ToggleTheme() {
+		if (props.theme === "business") {
+			props.setTheme("lightMode")
+			window.localStorage.setItem("theme", "lightMode")
+		} else {
+			props.setTheme("business")
+			window.localStorage.setItem("theme", "business")
+		}
+	}
+
 	return (
 		<div className="navbar h-[7%] bg-primary">
 			<div className="flex-1">
 				<Link href={"/"}>
-					<div className="btn btn-ghost normal-case text-xl text-primary-content">
+					<div className="btn btn-ghost normal-case rounded-xl text-xl text-primary-content">
 						Superbug AI
 					</div>
 				</Link>
 			</div>
-			<div className="btn btn-ghost">
-				<LightModeIcon className="text-2xl text-primary-content" />
+			<div className="btn btn-ghost rounded-xl" onClick={ToggleTheme}>
+				{props.theme === "lightMode" ? (
+					<LightModeIcon className="text-2xl text-primary-content" />
+				) : (
+					<DarkModeIcon className="text-2xl text-primary-content" />
+				)}
 			</div>
 		</div>
 	)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,19 +10,35 @@ export default function MyApp({ Component, pageProps }) {
 	const [apiUrl, setApiUrl] = useState("http://localhost:8080/fhir/")
 	const contextWrapper = { value: apiUrl, setter: setApiUrl }
 
+	// state for the theme
+	const [theme, setTheme] = useState("business")
+
 	useEffect(() => {
+		// reload api url from local storage if previously set
 		let currentApiUrl = window.localStorage.getItem("currentApiUrl")
 		if (currentApiUrl !== null) {
 			setApiUrl(currentApiUrl)
+		}
+
+		// reload the theme from local storage if previously set
+		let currentTheme = window.localStorage.getItem("theme")
+		if (currentTheme !== null) {
+			setTheme(currentTheme)
+		} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+			setTheme("business")
+			window.localStorage.setItem("theme", "business")
+		} else {
+			setTheme("lightMode")
+			window.localStorage.setItem("theme", "lightMode")
 		}
 	}, [])
 
 	return (
 		<LocalizationProvider dateAdapter={AdapterDayjs}>
-			<div data-theme="business" className="font-sans" id="themeWrapper">
+			<div data-theme={theme} className="font-sans" id="themeWrapper">
 				<ApiContext.Provider value={contextWrapper}>
 					<div className="flex flex-col h-screen min-w-screen">
-						<NavBar />
+						<NavBar theme={theme} setTheme={setTheme} />
 						<div className="flex flex-col h-[93%] overflow-auto w-full items-center bg-base-200">
 							<Component {...pageProps} />
 						</div>
