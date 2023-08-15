50c175e2e023f9ad5ab70f27c34311bf1685d5f8
Derek Huang <derekhuang343@gmail.com>
2023-08-15 15:43:56 +1000

    Base code for other grouped count api calls

---
+++ src/pages/api/location/groupedCount.ts
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+function getGroupedLocaCount(apiUrl: string, start: Date, end: Date) {
+	const apiCall: any = axios
+		.get(`${apiUrl}Location/$groupedLocationsCount`, {
+			params: { start: start, end: end },
+		})
+		.then((res) => {
+			if (res.hasOwnProperty("data")) {
+				return res.data
+			} else {
+				return []
+			}
+		})
+	return apiCall
+}
+
+// handler for any calls to this endpoint
+export default async function handler(req, res) {
+	const params = req.query
+
+	try {
+		const result = await getGroupedLocaCount(
+			params.apiUrl,
+			params.start,
+			params.end
+		)
+
+		res.status(200).json(result)
+	} catch (err) {
+		res.status(500).json(err)
+	}
+}
---
+++ src/pages/api/procedure/groupedCount.ts
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+function getGroupedProcCount(apiUrl: string, start: Date, end: Date) {
+	const apiCall: any = axios
+		.get(`${apiUrl}Procedure/$groupedProceduresCount`, {
+			params: { start: start, end: end },
+		})
+		.then((res) => {
+			if (res.hasOwnProperty("data")) {
+				return res.data
+			} else {
+				return []
+			}
+		})
+	return apiCall
+}
+
+// handler for any calls to this endpoint
+export default async function handler(req, res) {
+	const params = req.query
+
+	try {
+		const result = await getGroupedProcCount(
+			params.apiUrl,
+			params.start,
+			params.end
+		)
+
+		res.status(200).json(result)
+	} catch (err) {
+		res.status(500).json(err)
+	}
+}
