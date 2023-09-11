fcfe4f6e7eb72f6fc4b10d09e69c4ebd073ccf0c
Derek Huang <derek.huang@monash.edu>
2023-09-11 11:43:20 +1000

    Base addition of breadcrumbs

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,10 +1,96 @@
-function Breadcrumbs() {
-	return (
-		<div className="text-sm breadcrumbs">
+import Link from "next/link"
+import { useRouter } from "next/router"
+import React, { useEffect } from "react"
+
+const _defaultGetTextGenerator = (param, query) => null
+const _defaultGetDefaultTextGenerator = (path) => {
+	path = path.replace("/", "")
+	path = path.match(/[A-Z][a-z]+|[0-9]+/g).join(" ")
+	return path
+}
+
+const generatePathParts = (pathStr) => {
+	const pathWithoutQuery = pathStr.split("?")[0]
+	return pathWithoutQuery.split("/").filter((v) => v.length > 0)
+}
+
+function Breadcrumbs({
+	getTextGenerator = _defaultGetTextGenerator,
+	getDefaultTextGenerator = _defaultGetDefaultTextGenerator,
+}) {
+	const router = useRouter()
+
+	const breadcrumbs = React.useMemo(
+		function generateBreadcrumbs() {
+			const asPathNestedRoutes = generatePathParts(router.asPath)
+			const pathnameNestedRoutes = generatePathParts(router.pathname)
+
+			const crumblist = asPathNestedRoutes.map((subpath, idx) => {
+				// Pull out and convert "[post_id]" into "post_id"
+				const param = pathnameNestedRoutes[idx]
+					.replace("[", "")
+					.replace("]", "")
+				const href =
+					"/" + asPathNestedRoutes.slice(0, idx + 1).join("/")
+				return {
+					href,
+					textGenerator: getTextGenerator(param, router.query),
+					text: getDefaultTextGenerator(href),
+				}
+			})
+
+			return [{ href: "/", text: "Home" }, ...crumblist]
+		},
+		[
+			router.asPath,
+			router.pathname,
+			router.query,
+			getTextGenerator,
+			getDefaultTextGenerator,
+		]
+	)
+
+	console.log(breadcrumbs.length)
+
+	return breadcrumbs.length > 1 ? (
+		<div className="flex breadcrumbs my-2">
 			<ul>
-				<li>Home</li>
+				{breadcrumbs.map((crumb, idx) => (
+					<Crumb
+						{...crumb}
+						key={idx}
+						last={idx === breadcrumbs.length - 1}
+					/>
+				))}
 			</ul>
 		</div>
+	) : null
+}
+
+function Crumb({ text: defaultText, textGenerator, href, last = false }) {
+	const [text, setText] = React.useState(defaultText)
+
+	useEffect(() => {
+		if (!Boolean(textGenerator)) return setText(defaultText)
+
+		async function fetchData() {
+			const currText = await textGenerator()
+			setText(currText)
+		}
+
+		fetchData()
+	}, [defaultText, textGenerator])
+
+	if (last) {
+		return <li>{text}</li>
+	}
+
+	return (
+		<li>
+			<Link color="inherit" href={href}>
+				{text}
+			</Link>
+		</li>
 	)
 }
 
