e42935c255791e06160631728d47da1ea6d9cd04
Derek Huang <derek.huang@monash.edu>
2023-11-08 16:31:51 +1100

    Added code for AAD login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import OktaProvider from "next-auth/providers/okta"
 import Auth0Provider from "next-auth/providers/auth0"
+import AzureADProvider from "next-auth/providers/azure-ad"
 
 export const authOptions = {
 	// Configure one or more authentication providers
@@ -14,6 +15,11 @@ export const authOptions = {
 			clientId: process.env.AUTH0_CLIENT_ID,
 			clientSecret: process.env.AUTH0_CLIENT_SECRET,
 			issuer: process.env.AUTH0_ISSUER,
+		}),
+		AzureADProvider({
+			clientId: process.env.AZURE_AD_CLIENT_ID,
+			clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+			tenantId: process.env.AZURE_AD_TENANT_ID,
 		}),
 	],
 }
