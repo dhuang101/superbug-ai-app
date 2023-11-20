e6fd90f06ebb59b542806e380145d81b045035af
Derek Huang <derek.huang@monash.edu>
2023-11-20 16:40:58 +1100

    Testing gantt chart package

--- src/pages/infection-prevention/location/[resourceName]/index.tsx
+++ src/pages/infection-prevention/location/[resourceName]/index.tsx
@@ -1,9 +1,34 @@
 import { useRouter } from "next/router"
+import {
+	Gantt,
+	Task,
+	EventOption,
+	StylingOption,
+	ViewMode,
+	DisplayOption,
+} from "gantt-task-react"
+import "gantt-task-react/dist/index.css"
 import Breadcrumbs from "../../../../components/Breadcrumbs"
 
 function InfLocaSummary() {
 	// router
 	const router = useRouter()
+
+	let tasks: Task[] = [
+		{
+			start: new Date(2020, 1, 1),
+			end: new Date(2020, 1, 2),
+			name: "Idea",
+			id: "Task 0",
+			type: "task",
+			progress: 45,
+			isDisabled: true,
+			styles: {
+				progressColor: "#ffbb54",
+				progressSelectedColor: "#ff9e0d",
+			},
+		},
+	]
 
 	return (
 		<div className="flex flex-col w-8/12">
@@ -11,7 +36,9 @@ function InfLocaSummary() {
 			<article className="text-3xl mb-4 font-semibold">
 				{router.query.resourceName}
 			</article>
-			<div className="flex justify-center"></div>
+			<div className="flex justify-center">
+				<Gantt tasks={tasks} />
+			</div>
 		</div>
 	)
 }
