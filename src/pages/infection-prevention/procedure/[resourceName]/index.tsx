3f0ee9226a3cc53e6e1317cf9a0d1dd10f68404d
Derek Huang <derek.huang@monash.edu>
2024-01-09 23:44:02 +1100

    Additions to network graph

--- src/pages/infection-prevention/procedure/[resourceName]/index.tsx
+++ src/pages/infection-prevention/procedure/[resourceName]/index.tsx
@@ -1,11 +1,16 @@
 import { useRouter } from "next/router"
 import Breadcrumbs from "../../../../components/Breadcrumbs"
-import ReactFlow from "reactflow"
+import ReactFlow, { Background, Controls, MiniMap } from "reactflow"
 import "reactflow/dist/style.css"
 
 function InfPreSummary() {
 	// router
 	const router = useRouter()
+
+	console.log(
+		JSON.parse(decodeURIComponent(router.query.summaryData as string))
+	)
+
 	const initialNodes = [
 		{ id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
 		{ id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
@@ -18,8 +23,12 @@ function InfPreSummary() {
 			<article className="text-3xl mb-4 font-semibold">
 				{router.query.resourceName}
 			</article>
-			<div className="w-full h-[82vh]">
-				<ReactFlow nodes={initialNodes} edges={initialEdges} />
+			<div className="w-full h-[82vh] bg-[oklch(var(--s))]">
+				<ReactFlow nodes={initialNodes} edges={initialEdges}>
+					<Background color="oklch(var(--sc))" />
+					<MiniMap />
+					<Controls />
+				</ReactFlow>
 			</div>
 		</div>
 	)
