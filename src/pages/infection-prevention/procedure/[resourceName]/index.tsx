f2fe9237f696c5e585d9d95ffecdc8579dd7fbe6
Derek Huang <derek.huang@monash.edu>
2024-01-10 17:05:11 +1100

    Skeleton code for generating nodes and edges

--- src/pages/infection-prevention/procedure/[resourceName]/index.tsx
+++ src/pages/infection-prevention/procedure/[resourceName]/index.tsx
@@ -2,14 +2,47 @@ import { useRouter } from "next/router"
 import Breadcrumbs from "../../../../components/Breadcrumbs"
 import ReactFlow, { Background, Controls, MiniMap } from "reactflow"
 import "reactflow/dist/style.css"
+import { useEffect, useState } from "react"
 
 function InfPreSummary() {
 	// router
 	const router = useRouter()
+	const [nodes, setNodes] = useState([])
+	const [edges, setEdges] = useState([])
 
-	console.log(
-		JSON.parse(decodeURIComponent(router.query.summaryData as string))
-	)
+	// generate edges and node from data
+	useEffect(() => {
+		// parse data from query
+		let rawData = JSON.parse(
+			decodeURIComponent(router.query.summaryData as string)
+		)
+		// temp variables
+		let nodes = []
+		let edges = []
+
+		rawData.forEach((procedure) => {
+			console.log(procedure)
+
+			// checks if the current procedure's patient does not have a node
+			if (
+				!nodes.some((node) => {
+					node.id === procedure.patientId
+				})
+			) {
+				// creates a node for that patient
+				nodes.push({
+					id: procedure.patientId,
+					position: { x: 0, y: 0 },
+					data: { label: procedure.patientId },
+				})
+			}
+
+			// generates edges for the given procedure
+		})
+		// set data to state
+		setNodes(nodes)
+		setEdges(edges)
+	}, [])
 
 	const initialNodes = [
 		{ id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
@@ -24,7 +57,7 @@ function InfPreSummary() {
 				{router.query.resourceName}
 			</article>
 			<div className="w-full h-[82vh] bg-[oklch(var(--s))]">
-				<ReactFlow nodes={initialNodes} edges={initialEdges}>
+				<ReactFlow nodes={nodes} edges={edges}>
 					<Background color="oklch(var(--sc))" />
 					<MiniMap />
 					<Controls />
