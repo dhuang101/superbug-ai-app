eeb8e87c3144bfb9177474193223d99bd800606c
Derek Huang <derek.huang@monash.edu>
2024-04-15 15:55:50 +1000

    Adding test scope to provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -14,6 +14,9 @@ export const authOptions = {
 			clientId: process.env.AZURE_AD_CLIENT_ID,
 			clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
 			tenantId: process.env.AZURE_AD_TENANT_ID,
+			authorization: {
+				params: { scope: "user.read" },
+			},
 		}),
 	],
 }
